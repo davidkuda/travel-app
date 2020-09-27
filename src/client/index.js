50b8cff9956e9aa214c703a90a8f67e4c98bd0af
David Kuda <david.kuda@pm.me>
2020-09-27 23:28:22 +0200

    feat: installs file-loader and url-loader

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,30 @@
+// import CSS
 import './styles/base.scss';
 
+// import own modules
 import {printName} from './js/printName';
 import {handleSubmit} from './js/handleSubmit';
+
+// import assets
+ import img from './img/travel-alps.jpg';
+
+ // functions playground
+function component() {
+    const element = document.createElement('div');
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = img;
+    element.appendChild(myIcon);
+    return element;
+}
+
+document.body.appendChild(component());
 
 printName();
 
 console.log('this message will be in output: FENDER STRATOCASTER!');
 
+// global Client exports
 export {
     handleSubmit
 }
--- webpack.dev.js
+++ webpack.dev.js
@@ -15,18 +15,25 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: '/\.js$/',
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        }, 
-        {
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+                test: '/\.js$/',
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ]
+            }
+        ]
     },
     plugins: [new HtmlWebpackPlugin({
         template: "./src/client/views/index.html",
