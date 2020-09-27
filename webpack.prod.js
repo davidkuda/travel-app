f316380204a7271d313dddb4a1e57332883af630
David Kuda <david.kuda@pm.me>
2020-09-27 18:10:16 +0200

    feat: uses CleanWebpackPlugin for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     output: {
         filename: 'main.js',
@@ -14,7 +17,18 @@ module.exports = {
             loader: "babel-loader"
         }]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/client/views/index.html",
-    })]
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/views/index.html",
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    ]
 };
