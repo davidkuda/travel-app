384a31eeee53b8b1b1418705efc1607c99bdac70
David Kuda <david.kuda@pm.me>
2020-09-27 17:53:55 +0200

    feat: splits webpack into dev and prod

---
+++ webpack.dev.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'sourcempap',
+    entry: './src/client/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/client/views/index.html",
+    })]
+};
