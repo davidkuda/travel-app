3a6b410d19c428fbbc9e2daa22e921bf3b008072
David Kuda <david.kuda@pm.me>
2020-10-02 23:06:32 +0200

    feat: requires city as parameter

--- src/client/js/apiPixabay.js
+++ src/client/js/apiPixabay.js
@@ -1,7 +1,7 @@
-export const getPixabayUrl = () => {
+export const getPixabayUrl = (geonamesCity) => {
     const baseUrl = 'https://pixabay.com/api/';
     const key = '?key=18504235-b5f67f1c60c6a4f6cf2f7d060';
-    const city = '&q=berlin';
+    const city = '&q=' + geonamesCity;
     const imgtype = '&image_type=photo';
     const orientation = '&orientation=horizontal';
     const pixabayUrl = `${baseUrl}${key}${city}${imgtype}${orientation}`;
