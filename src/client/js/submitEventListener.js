f8c579b773c43bae14bd68584c6bd1e72a16c100
David Kuda <david.kuda@pm.me>
2020-10-02 23:08:00 +0200

    feat: loads new markup after click on submit

--- src/client/js/submitEventListener.js
+++ src/client/js/submitEventListener.js
@@ -1,46 +1,47 @@
  // variables
-
  const userInputForm = document.getElementById('userInputForm');
-
- // event listeners
 
  userInputForm.addEventListener('submit', async () => {
 
-     console.log('%c hello from module !', 'color: green');
-     console.table({location: 'module'});
-     const message = 'Origin is submitEventListener.js';
-     console.log({message});
-
-    let userInput = getUserInput();
-    let formContainer = document.getElementById('form-container'); //TODO: Change name to 'update main content'
-    updateUi(formContainer, markupLoader); //TODO: make markup class and get child loader
-
+    let userInput = Client.getUserInput();
+    console.table({userInput: userInput});
     let dates = {
-        startDate: userInput.date,
-        endDate: null //TODO: get endDate from user
-    };
+        startDate: userInput.startDate,
+        endDate: userInput.endDate
+    } // TODO: change date format
+
+    Client.updateUi(Client.markupLoader); //TODO: make markup class and get child loader
+
+    // get Geonames API Data
 
     let apiData = {
-        geonames: await apiCall(await getGeonamesUrl(userInput.destination))
+        geonames: await Client.apiCall(await Client.getGeonamesUrl(userInput.destination))
     };
 
-    let pixabayData = await apiCall(await getPixabayUrl());
+    // get Pixabay API Data
+
+    let geonamesCity = apiData.geonames.geonames[0].name;
+    let pixabayData = await Client.apiCall(await Client.getPixabayUrl(geonamesCity));
     apiData['pixabay'] = pixabayData;
+
+    // get Weatherbit API Data
+
+    /*
 
     let latitude = apiData.geonames.geonames[0].lat;
     let longitude = apiData.geonames.geonames[0].lng;
-    let startDate = '2019-10-20';
-    let endDate = '2019-10-21';
-    let weatherbit = await apiCall(await getWeatherbitUrl(latitude, longitude, startDate, endDate));
+    let startDate = userInput.startDate;
+    let endDate = userInput.endDate;
+    let weatherbit = await Client.apiCall(await Client.getWeatherbitUrl(latitude, longitude, startDate, endDate));
     apiData['weatherbit'] = weatherbit;
 
+    */
 
-    console.log('geonames Data: ', apiData.geonames.geonames[0]);
-    console.log('weatherbit Data: ', apiData.weatherbit);
-    console.log('pixabay Data: ', apiData.pixabay.hits[0].webformatURL);
-    console.log('apiData: ', apiData);
+    console.log(apiData)
 
-    let travelResponse = await sampleResponse(apiData, dates);
-    updateUi(formContainer, travelResponse);
+    let travelResponse = await Client.sampleResponse(apiData, dates);
+    Client.changeBackground(apiData.pixabay.hits[0].webformatURL);
+    // Client.changeBackground(apiData.pixabay.hits[0].largeImageURL);
+    Client.updateUi(travelResponse);
 
  })
