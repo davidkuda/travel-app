593c7eb417ac457831939dcdfdd558062c62e77a
David Kuda <david.kuda@pm.me>
2020-10-02 23:07:11 +0200

    feat: shows city and country as h1

--- src/client/js/markupResponse.js
+++ src/client/js/markupResponse.js
@@ -1,5 +1,6 @@
 export const sampleResponse = (apiData = {}, dates = {}) => {
     return `
-    <p>You are travelling to ${apiData.geonames.geonames[0].name}, ${apiData.geonames.geonames[0].countryName}, on ${dates.startDate}</p>
-    <img src="${apiData.pixabay.hits[0].webformatURL}" alt="picture of ${apiData.geonames.geonames[0].name}" class="city-pic" />`;
+    <h1>${apiData.geonames.geonames[0].name}, ${apiData.geonames.geonames[0].countryName}</h1>
+    <p/>${dates.startDate}</p>
+    `;
 };
