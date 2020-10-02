6dcebdb11ba7f07c915a9b725b282a95c9397928
David Kuda <david.kuda@pm.me>
2020-10-02 23:23:10 +0200

    fix: prevents repeat and ensures cover size

--- src/client/js/changeBackground.js
+++ src/client/js/changeBackground.js
@@ -1,5 +1,7 @@
 export const changeBackground = source => {
     console.log('hello from changeBackground.js!')
     console.log({source})
-    document.body.style.background = `url(${source})`;
+    document.body.style.background = `url(${source}) center`;
+    // document.body.style.background = `url(${source}) bottom`;
+    document.body.style.backgroundSize = `cover`;
 };
