f82a18264fca690fcc25be9b1b3b55ddf1b727dd
David Kuda <david.kuda@pm.me>
2020-10-04 23:36:02 +0200

    feat: shows weather icon in the forecast

--- src/client/js/markup.js
+++ src/client/js/markup.js
@@ -23,18 +23,23 @@ export const markupResponse = (apiData = {}, dates = {}) => {
     <div class="day">
         <span class="date">${dayjs(apiData.weatherbit.data[0].datetime).format('MMM. D')}</span>
         <span class="temperature">${apiData.weatherbit.data[0].min_temp} to ${apiData.weatherbit.data[0].max_temp}°C</span>
+        <img class="icon" src=https://www.weatherbit.io/static/img/icons/${apiData.weatherbit.data[0].weather.icon}.png />
         <span class="description">${apiData.weatherbit.data[0].weather.description}</span>
     </div>
     <div class="day">
         <span class="date">${dayjs(apiData.weatherbit.data[1].datetime).format('MMM. D')}</span>
-        <span class="temperature">${apiData.weatherbit.data[1].min_temp} to ${apiData.weatherbit.data[0].max_temp}°C</span>
+        <span class="temperature">${apiData.weatherbit.data[1].min_temp} to ${apiData.weatherbit.data[1].max_temp}°C</span>
+        <img class="icon" src=https://www.weatherbit.io/static/img/icons/${apiData.weatherbit.data[1].weather.icon}.png />
         <span class="description">${apiData.weatherbit.data[1].weather.description}</span>
     </div>
     <div class="day">
         <span class="date">${dayjs(apiData.weatherbit.data[2].datetime).format('MMM. D')}</span>
-        <span class="temperature">${apiData.weatherbit.data[2].min_temp} to ${apiData.weatherbit.data[0].max_temp}°C</span>
+        <span class="temperature">${apiData.weatherbit.data[2].min_temp} to ${apiData.weatherbit.data[2].max_temp}°C</span>
+        <img class="icon" src=https://www.weatherbit.io/static/img/icons/${apiData.weatherbit.data[2].weather.icon}.png />
         <span class="description">${apiData.weatherbit.data[2].weather.description}</span>
     </div>
 </div>
     `;
 };
+
+' <img src="img_girl.jpg" alt="Girl in a jacket" width="500" height="600"> '
