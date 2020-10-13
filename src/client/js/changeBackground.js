0ed85f1c58513c0f482207d58d727393f6a3f134
David Kuda <david.kuda@pm.me>
2020-10-13 21:29:22 +0200

    Refactor: Remove unnecessary console.log

--- src/client/js/changeBackground.js
+++ src/client/js/changeBackground.js
@@ -1,5 +1,4 @@
 export const changeBackground = source => {
-    console.log('hello from changeBackground.js!')
     console.log({source})
     document.body.style.background = `url(${source}) center`;
     // document.body.style.background = `url(${source}) bottom`;
