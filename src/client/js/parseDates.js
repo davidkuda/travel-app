01b06d6f45b5d640ef75f17336bf73004b538e17
David Kuda <david.kuda@pm.me>
2020-10-29 19:58:01 +0100

    refactor: Parametrize startdate and enddate

--- src/client/js/parseDates.js
+++ src/client/js/parseDates.js
@@ -1,22 +1,18 @@
-export const parseDates = (userInput) => {
+export const parseDates = (startDate, endDate) => {
 
     const dayjs = require('dayjs');
     const relativeTime = require('dayjs/plugin/relativeTime');
     dayjs.extend(relativeTime);
 
-
     let parsedDates = {
         dayjs: {
-            startDate: dayjs(userInput.startDate, ['YYYY-MM-DD', 'DD.MM.YYYY']),
-            endDate: dayjs(userInput.endDate, ['YYYY-MM-DD', 'DD.MM.YYYY'])
+            startDate: dayjs(startDate, ['YYYY-MM-DD', 'DD.MM.YYYY']),
+            endDate: dayjs(endDate, ['YYYY-MM-DD', 'DD.MM.YYYY'])
         },
-        startDate: dayjs(userInput.startDate, ['YYYY-MM-DD', 'DD.MM.YYYY']).format('DD. MMMM YYYY'),
-        endDate: dayjs(userInput.endDate, ['YYYY-MM-DD', 'DD.MM.YYYY']).format('DD. MMMM YYYY'),
-        countdown: dayjs(userInput.startDate, ['YYYY-MM-DD', 'DD.MM.YYYY']).fromNow()
+        startDate: dayjs(startDate, ['YYYY-MM-DD', 'DD.MM.YYYY']).format('DD. MMMM YYYY'),
+        endDate: dayjs(endDate, ['YYYY-MM-DD', 'DD.MM.YYYY']).format('DD. MMMM YYYY'),
+        countdown: dayjs(startDate, ['YYYY-MM-DD', 'DD.MM.YYYY']).fromNow()
     }
-    
-    console.log('%c PARSED DATES:', 'color: green');
-    console.log(parsedDates)
 
     return parsedDates
 };
