bdc3053da196938d2371b7533f6ba77a44b93192
David Kuda <david.kuda@pm.me>
2020-10-29 19:46:51 +0100

    feat: Add cors to app

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+// const { markup } = require("../client");
 const app = express();
 
 app.use(express.static('dist'))
+
+const cors = require('cors');
+app.use(cors())
 
 app.get("/", function (req, res) {
   res.send('dist/index.html');
@@ -10,3 +14,23 @@ app.get("/", function (req, res) {
 app.listen(3000, function() {
     console.log("app listening on port 3000!");
 });
+
+// create an object to store all data that is available to front end 
+const data = {};
+
+// Deliver data to front end through endpoint
+app.get('/response', (req, res) => {
+  res.send(data)
+});
+
+// create callback for post request to /request
+
+const getUserInput = (req, res) => {
+  console.log('caught post request')
+  console.log(req.body)
+  // data.push(req.body)
+}
+
+// create an endpoint to receive input from front end
+app.post('/request', getUserInput);
+
