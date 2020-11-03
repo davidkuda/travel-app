963b5bb3e9a611556c5dddbbdd869bc2241f3f1a
David Kuda <david.kuda@pm.me>
2020-11-04 00:47:08 +0100

    test: Update test to new functions

--- src/client/js/__tests__/parseDates.spec.js
+++ src/client/js/__tests__/parseDates.spec.js
@@ -1,7 +1,7 @@
 const { parseDates } = require('../parseDates');
 
 test('Should return object with dates', () => {
-    const input = parseDates('2020-11-11', '2020-11-12');
-    const output = {dayjs: {startDate: "11.11.2020", endDate: "12.11.2020"}, startDate: "11. November 2020", endDate: "12. November 2020", countdown: "in 7 days"}
+    const input = parseDates('10.11.2020', '20.11.2020');
+    const output = {dayjs: {endDate: "20.11.2020", startDate: "10.11.2020"}, startDate: "10. November 2020", endDate: "20. November 2020", countdown: "in 6 days", duration: 10}
     expect(input).toEqual(output);
 });
