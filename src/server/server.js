b6ab3f137d34bdefbfa7c8881821eb34c35e6399
David Kuda <david.kuda@pm.me>
2020-11-04 00:32:20 +0100

    fix: Remove get and post routes

--- src/server/server.js
+++ src/server/server.js
@@ -18,18 +18,3 @@ app.get("/", function (req, res) {
 app.listen(3000, function() {
     console.log("app listening on port 3000!");
 });
-
-// create an object to store all data that is available to front end 
-const inputData = {};
-
-// Deliver data to front end through endpoint
-app.get('/response', (req, res) => {
-  res.send(data)
-});
-
-// create an endpoint to receive input from front end
-app.post('/post', async (req, res) => {
-  let data = req.body;
-  console.log(data)
-  inputData.push(data)
-});
