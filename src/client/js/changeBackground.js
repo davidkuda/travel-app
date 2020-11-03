96a26274c5a694d47afa4dabb1e382eb1c52de21
David Kuda <david.kuda@pm.me>
2020-11-04 00:39:02 +0100

    refactor: Remove console log

--- src/client/js/changeBackground.js
+++ src/client/js/changeBackground.js
@@ -1,5 +1,4 @@
 export const changeBackground = source => {
-    console.log({source})
     document.body.style.background = `url(${source}) center`;
     // document.body.style.background = `url(${source}) bottom`;
     document.body.style.backgroundSize = `cover`;
