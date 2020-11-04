a95939ec885f4e33c3fd437184269b4d0539b3c8
David Kuda <david.kuda@pm.me>
2020-11-04 22:43:38 +0100

    fix: Adjust expected output

--- src/client/js/__tests__/parseDates.spec.js
+++ src/client/js/__tests__/parseDates.spec.js
@@ -2,6 +2,6 @@ const { parseDates } = require('../parseDates');
 
 test('Should return object with dates', () => {
     const input = parseDates('10.11.2020', '20.11.2020');
-    const output = {dayjs: {endDate: "20.11.2020", startDate: "10.11.2020"}, startDate: "10. November 2020", endDate: "20. November 2020", countdown: "in 6 days", duration: 10}
+    const output = {dayjs: {endDate: "20.11.2020", startDate: "10.11.2020"}, startDate: "10. November 2020", endDate: "20. November 2020", countdown: "in 5 days", duration: 10}
     expect(input).toEqual(output);
 });
