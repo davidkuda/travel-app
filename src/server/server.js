334971e7d3516f5292a0a81127a12568f6165f83
David Kuda <david.kuda@pm.me>
2020-11-04 22:41:49 +0100

    feat: Split listening to port into separate file for async test with supertest and jest to work

--- src/server/server.js
+++ src/server/server.js
@@ -15,10 +15,8 @@ app.get("/", function (req, res) {
   res.send('dist/index.html');
 });
 
-app.listen(3000, function() {
-    console.log("app listening on port 3000!");
-});
-
 app.get('/response', (req, res) => {
-  res.send('THIS IS SPARTA!')
+  res.json({leonidas: 'THIS IS SPARTA!'})
 })
+
+module.exports = app
---
+++ src/server/start.js
@@ -0,0 +1,6 @@
+const app = require('./server.js')
+
+
+app.listen(3000, function() {
+    console.log("app listening on port 3000!");
+});
